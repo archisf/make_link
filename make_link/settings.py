74d7503eb990a2b6bd82a5f9f1053a960eb2d9e0
Arkady <arkadyaristov@gmail.com>
2016-09-16 16:15:02 +0500

    Update settings.py

--- make_link/settings.py
+++ make_link/settings.py
@@ -13,13 +13,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$8pzikeeji=#-ld0v(ie5j7+nzv+=lv*xht=an&5(#kosygulp'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
